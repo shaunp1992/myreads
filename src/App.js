6679cab5019bf7120caf0ae18ab4bac20cc5b29b
Shaun Pearce <shaunpearce1992@gmail.com>
2017-08-21 23:22:56 -0700

    feat: Search/filter books by query
    Added the ability to search books by title or author on the search page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import BookShelf from './BookShelf.js'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
+    books : [],
+    showSearchPage: true,
   }
+  
+   componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({ books })
+        })   
+    }
+
+    
 
   render() {
+
     return (
       <div className="app">
         
@@ -26,7 +30,7 @@ class BooksApp extends React.Component {
         </Route>    
         
         <Route path='/search' render={( {history} ) => (
-              <SearchBooks></SearchBooks>
+              <SearchBooks books={this.state.books}></SearchBooks>
             )}>
         </Route>    
             
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,22 +1,68 @@
 import React, { Component } from 'react'
 import { Link} from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
 
 class SearchBooks extends Component {
     
+    state = {
+        query: '',
+    }
+        
+    updateQuery = (query => {
+        this.setState({query: query})
+    })
+    
     render(){
+        
+        var showingBooks
+        var { books } = this.props
+        var { query }= this.state
+        
+        if(query){
+            var match = new RegExp(escapeRegExp(query), 'i')
+            showingBooks = books.filter((book) => match.test(book.title) || match.test(book.authors))
+        }
+        else{
+            showingBooks = books
+        }
+        
         return (
-        <div className="search-books">
-            <div className="search-books-bar">
-                  <Link to="/" className="close-search">Close</Link>
-                
-                  <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author"/>
-                  </div>
-            </div>
-            <div className="search-books-results">
-                    <ol className="books-grid"></ol>
-            </div>
-        </div> 
+            <div className="search-books">
+                <div className="search-books-bar">
+                      <Link to="/" className="close-search">Close</Link>
+                      <div className="search-books-input-wrapper">
+                          <input type="text" placeholder="Search by title or author" value={query} onChange={(event) => this.updateQuery(event.target.value)}/>
+                      </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {showingBooks.map((book) => ( 
+                            <li key={book.id}>
+                            <div className="book">
+                              <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className="book-shelf-changer">
+                                  <select>
+                                    <option value="none" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{book.title}</div>
+                              <ul className="book-authors">
+                                  {book.authors.map((author, index) => ( 
+                                      <li key={index} className="book-author">{author}</li>
+                                  ))}
+                              </ul>    
+                            </div>
+                          </li>
+                          ))}
+                    </ol>
+                </div>
+            </div> 
         )
     }
   
