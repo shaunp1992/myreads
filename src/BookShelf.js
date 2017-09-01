98d1811a4023fd067e3f2c4621d0cb407f03ccb3
Shaun Pearce <shaunpearce1992@gmail.com>
2017-09-01 01:08:41 -0700

    Style: Added tab styling
    Added tab styling for shelve tabs

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import sortBy from 'sort-by'
 import Book from './Book.js'
+import $ from 'jquery';
 
 
 class BookShelf extends Component {
     
     state ={
         sort : 'title',
-        visibleShelf: 'all'
+        visibleShelf: 'all',
+        active: false
+    }
+    
+    componentDidMount() {
+       var activeWidth = $('.active-tab').outerWidth()
+       document.getElementById("tab-underline").style.width = activeWidth + "px"     
+       
+       var activeLeft = ($(".active-tab" ).position().left) - 175
+       document.getElementById("tab-underline").style.marginLeft = activeLeft + "px"   
+    }
+
+    componentDidUpdate() {
+       var activeWidth = $('.active-tab').outerWidth()
+       document.getElementById("tab-underline").style.width = activeWidth + "px"     
+       
+       var activeLeft = ($(".active-tab" ).position().left) - 175
+       document.getElementById("tab-underline").style.marginLeft = activeLeft + "px"     
+       
     }
 
     onChangeSortOrder(sortOption) {
@@ -17,20 +36,16 @@ class BookShelf extends Component {
          })
     }
 
-    onChangeVisibleShelf(){
-//         this.setState({ 
-//             visibleShelf: visibleShelfSelection 
-//         })
-        console.log("clicked");
-    }
-
     onChangeVisibleShelf (visibleShelfSelection){
         this.setState({ 
-             visibleShelf: visibleShelfSelection 
+             visibleShelf: visibleShelfSelection,
          })
       }
 
     render(){
+        
+       // console.log("testing");
+        
         
         var { sort, visibleShelf } = this.state
         var { books, onChangeShelf, onChangeRating, onChangeVisibleShelf, onClicky } = this.props
@@ -51,12 +66,16 @@ class BookShelf extends Component {
             
                 
             <div className="bookshelf-control-bar">
-                <div className="bookshelf-tab-controls">
-                    <div id="all" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>All</div>
-                    <div id="read" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Read</div>
-                    <div id="currentlyReading" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Currently Reading</div>
-                    <div id="wantToRead" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Want To Read</div>
+                <div className="bookshelf-tab-controls-container">
+                    <div className="bookshelf-tab-controls">
+                        <div id="all" className={"shelf-tab " + (visibleShelf === 'all' ? 'active-tab': '')} onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>All</div>
+                        <div id="read" className={"shelf-tab " + (visibleShelf === 'read' ? 'active-tab': '')} onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Read</div>
+                        <div id="currentlyReading" className={"shelf-tab " + (visibleShelf === 'currentlyReading' ? 'active-tab': '')} onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Reading</div>
+                        <div id="wantToRead" className={"shelf-tab " + (visibleShelf === 'wantToRead' ? 'active-tab': '')} onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Want To Read</div>
+                    </div>
+                    <hr id="tab-underline"/>
                 </div>
+                
                 
                 <div className="bookshelf-sorting-controls">
                     <select value={sort} onChange={(e) => this.onChangeSortOrder(e.target.value)}>
@@ -64,6 +83,7 @@ class BookShelf extends Component {
                             <option value="authors">Authors</option>
                             <option value="rating">Rating</option>
                     </select>
+                   
                 </div>   
             </div>    
              
