7861ca062b37f2f77f67e2b7194d48d58ec43e61
peash02 <Shaun.Pearce@contractor.ca.com>
2017-08-31 19:43:25 -0700

    Feat: Added tabs for viewing shelves
    Added the ability to change which shelves the user is viewing

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,19 +7,33 @@ import Book from './Book.js'
 class BookShelf extends Component {
     
     state ={
-        sort : 'title'
+        sort : 'title',
+        visibleShelf: 'all'
     }
-    
+
     onChangeSortOrder(sortOption) {
          this.setState({ 
              sort: sortOption 
          })
     }
 
+    onChangeVisibleShelf(){
+//         this.setState({ 
+//             visibleShelf: visibleShelfSelection 
+//         })
+        console.log("clicked");
+    }
+
+    onChangeVisibleShelf (visibleShelfSelection){
+        this.setState({ 
+             visibleShelf: visibleShelfSelection 
+         })
+      }
+
     render(){
         
-        var { sort } = this.state
-        var { books, onChangeShelf, onChangeRating } = this.props
+        var { sort, visibleShelf } = this.state
+        var { books, onChangeShelf, onChangeRating, onChangeVisibleShelf, onClicky } = this.props
         
         var read = books.filter(book => book.shelf === "read")
         var reading = books.filter(book => book.shelf === "currentlyReading")
@@ -29,63 +43,81 @@ class BookShelf extends Component {
         wantTo.sort(sortBy(sort))
         read.sort(sortBy(sort))
         
-        var options = [
-          { value: 'title', label: 'Title' },
-          { value: 'authors', label: 'Author' }
-        ];
-
         return (
          <div className="list-books">
             <div className="list-books-title">
-              <h1>MyReads</h1>
+              <h1>My Reads App</h1>
             </div>
             
-            <div className="bookshelf-controls">
-                <select value={sort} onChange={(e) => this.onChangeSortOrder(e.target.value)}>
-                        <option value="title">Title</option>
-                        <option value="authors">Author/s</option>
-                        <option value="rating">Rating</option>
-                </select>
+                
+            <div className="bookshelf-control-bar">
+                <div className="bookshelf-tab-controls">
+                    <div id="all" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>All</div>
+                    <div id="read" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Read</div>
+                    <div id="currentlyReading" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Currently Reading</div>
+                    <div id="wantToRead" onClick={(e) => this.onChangeVisibleShelf(e.target.id)}>Want To Read</div>
+                </div>
+                
+                <div className="bookshelf-sorting-controls">
+                    <select value={sort} onChange={(e) => this.onChangeSortOrder(e.target.value)}>
+                            <option value="title">Title</option>
+                            <option value="authors">Authors</option>
+                            <option value="rating">Rating</option>
+                    </select>
+                </div>   
             </div>    
+             
                 
             <div className="list-books-content">
               <div>
-                 <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {read.map((book) => (
-                              <li key={book.id}>
-                                    <Book book={book} onChangeShelf={this.props.onChangeShelf} onChangeRating={this.props.onChangeRating} ></Book>
-                              </li>
-                        ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {reading.map((book) => (
-                          <li key={book.id}>
-                                <Book book={book} onChangeShelf={this.props.onChangeShelf}></Book>
-                          </li>
-                        ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {wantTo.map((book) => (
-                              <li key={book.id}>
-                                    <Book book={book} onChangeShelf={this.props.onChangeShelf}></Book>
-                              </li>
-                        ))}
-                    </ol>
-                  </div>
-                </div>
+                  
+                  {visibleShelf === 'read' || visibleShelf === 'all' ? 
+                      <div className="bookshelf">
+                        <h2 className="bookshelf-title">Read</h2>
+                          <div className="bookshelf-books">
+                            <ol className="books-grid">
+                              {read.map((book) => (
+                                      <li key={book.id}>
+                                            <Book book={book} onChangeShelf={this.props.onChangeShelf} onChangeRating={this.props.onChangeRating} ></Book>
+                                      </li>
+                                ))}
+                            </ol>
+                          </div>
+                        </div>:null
+                  }
+                  
+                  
+                 {visibleShelf === 'currentlyReading' || visibleShelf === 'all' ? 
+                      <div className="bookshelf">
+                          <h2 className="bookshelf-title">Currently Reading</h2>
+                          <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                {reading.map((book) => (
+                                  <li key={book.id}>
+                                        <Book book={book} onChangeShelf={this.props.onChangeShelf}></Book>
+                                  </li>
+                                ))}
+                            </ol>
+                           </div>
+                    </div>:null
+                 }
+                  
+                 {visibleShelf === 'wantToRead' || visibleShelf === 'all' ? 
+                    <div className="bookshelf">
+                      <h2 className="bookshelf-title">Want to Read</h2>
+                      <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {wantTo.map((book) => (
+                                  <li key={book.id}>
+                                        <Book book={book} onChangeShelf={this.props.onChangeShelf}></Book>
+                                  </li>
+                            ))}
+                        </ol>
+                      </div>
+                     </div>:null
+                 }
+                  
+                  
               </div>
             </div>
             <div className="open-search">
